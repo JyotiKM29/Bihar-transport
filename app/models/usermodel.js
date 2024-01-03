566e9acadb9d0f642939ea7381bd23c9f9afbb81
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-01-04 04:42:57 +0530

    Updated the schema of admin or owner
    Please let me know if there's anything else.
    
    Co-Authored-By: Abhishek Gautam <Gautam.abhishek7100@gmail.com>

--- app/models/usermodel.js
+++ app/models/usermodel.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-  name: { type: String },
+  name: { type: String, required:true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  isemailVerified: { type: Boolean, default: false },
+  isphoneVerified: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  isOwner:{type:Boolean, default:false},
 },{timestamps:true});
 
 
