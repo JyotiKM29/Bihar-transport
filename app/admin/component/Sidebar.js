7ef3c328579dfb5118244e46aa2a8f9dcde170e1
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-01-13 21:45:03 +0530

    Updated role

--- app/admin/component/Sidebar.js
+++ app/admin/component/Sidebar.js
@@ -39,6 +39,7 @@ const Sidebar = () => {
   const { user } = useContext(UserContext);
   const userName = user?.name || "User Name";
   const userFirstName = userName.split(" ")[0];
+  const role = user ?.isOwner ? "Owner" : "Admin";
 
   const [selectedButton, setSelectedButton] = useState();
 
@@ -106,7 +107,7 @@ const Sidebar = () => {
           <button className="flex w-full items-center justify-between  p-4 py-2 ">
             <div className="flex flex-col items-start">
               <h2 className="text-md text-left"> {userFirstName}</h2>
-              <p className="text-sm text-gray-400">Admin</p>
+              <p className="text-sm text-gray-400">{role }</p>
             </div>
             <MdAccountCircle className="h-6 w-6 text-gray-400 md:h-8 md:w-8" />
           </button>
