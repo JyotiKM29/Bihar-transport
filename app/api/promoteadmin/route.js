1bd189ccd5397fa526df6e642d8a80adfd6369da
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-01-26 02:05:06 +0530

    Added api to authorize as owner

---
+++ app/api/promoteadmin/route.js
@@ -0,0 +1,57 @@
+import user from "../../models/usermodel";
+import connectDB from "../../middleware/connectDB";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  try {
+    await connectDB();
+    const { _id, id } = await req.json();
+
+    const owner = await user.findOne({ _id });
+
+    if (!owner) {
+      return Response.json(
+        {
+          message: "not found",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    if (!owner.isOwner) {
+      return Response.json({
+        message: "You're not allowed to authorize anyone",
+        status: 400,
+        contentType: "application/json",
+      });
+    }
+
+    // if admin
+
+    const Admin = await user.findOne({ _id: id });
+    if (!Admin) {
+      return Response.json({
+        message: "User does not exist",
+        status: 400,
+        contentType: "application/json",
+      });
+      }
+      
+      Admin.isOwner = true;
+      // Append the new login history entry
+      Admin.adminDetails.push({ assignedBy: owner.name });
+      await Admin.save();
+
+      return Response.json({ msg: "Admin Authorized", Admin, status: 200 });
+    
+  } catch (error) {
+    console.log("error at forget password api route", error);
+    return Response.json({ msg: error.message, status: 404 });
+  }
+}
+
+export function GET(req) {
+  return Response.json({ msg: "this method is not allowed", status: 400 });
+}
