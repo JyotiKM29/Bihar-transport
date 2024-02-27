f09583ebb5a13d0297d96e7707d8297d97ba8ae6
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-02-27 14:21:01 +0530

    Added api to get one payment voucher

---
+++ app/api/accounting/getOnePaymentVoucher/[_id]/route.js
@@ -0,0 +1,21 @@
+import connectDB from "../../../../middleware/connectDB";
+import paymentVoucher from "../../../../models/accounting/voucher";
+
+export async function GET(req, context) {
+    
+    try {
+        const { _id } = context.params;
+        console.log(_id);
+        await connectDB();
+
+        const paymentVoucherData = await paymentVoucher.findOne({ _id });
+        return Response.json({ message: "Payment Voucher Details", data: paymentVoucherData }, { status: 200 });
+
+    } catch (error) {
+        console.log(error);
+        return Response.json({ message: error.message }, { status: 400 });
+
+    }
+
+}
+
