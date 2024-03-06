aa57964bc128b5950dc56e7d17306ea4f19402d6
JyotiKM29 <jyotikumari4442@gmail.com>
2024-03-07 01:08:17 +0530

    Booking Report is completed

---
+++ app/admin/report/bookingReport/page.js
@@ -0,0 +1,37 @@
+'use client'
+import React from "react";
+import { Button } from "../../../components/ui/button";
+import SearchBooking from './SearchBooking'
+import { Backpack, ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const BookingReport = () => {
+  const router = useRouter();
+
+  function handleBack(){
+    router.back()
+  }
+  return (
+    <div
+    className="max-h flex 
+    w-full  flex-col  gap-3 space-y-2 rounded-2xl 
+     bg-white px-4 py-4 shadow-md md:p-6 xl:h-[95%]"
+  >
+  <div className="flex ">
+
+ 
+  
+    <h1 className="flex-1 text-2xl xl:text-3xl text-center lg:block ">Booking Report</h1>
+    <Button  variant='secondary' className='flex gap-2 self-end' onClick={handleBack}>
+    <ChevronLeft />
+     Back
+  </Button>
+    </div>
+    <SearchBooking />
+    
+  </div>
+  );
+};
+
+export default BookingReport;
+
--- app/admin/report/page.js
+++ app/admin/report/page.js
@@ -1,29 +1,29 @@
 "use client";
 import React, { useState } from "react";
 
-import BookingReport from "./booking-report/page";
+import BookingReport from "./bookingReport/page";
 import VehicleReport from "./vehicle-report/page";
-import InvoiceReport from "./invoice-report/page";
+import InvoiceReport from "./stateReport/page";
 import { Button } from "../../components/ui/button";
+import Link from "next/link";
 
 function Report() {
   const [showBooking, setShowBooking] = useState(true);
 
   return (
     <div className="min-h-[90vh] w-full rounded-2xl  bg-white p-6 shadow-sm ">
-      <h5 className="mb-4 text-center text-base  md:text-lg xl:mb-8">
-        Search report from below filters :
-      </h5>
+     
       <div className="flex flex-col gap-6 xl:flex-row">
-        {showBooking ? (
+        {/* {showBooking ? (
           <Button onClick={() => setShowBooking(!showBooking)}>
             Booking Report
           </Button>
         ) : (
           <BookingReport />
-        )}
-        {/* <VehicleReport />
-        <InvoiceReport /> */}
+        )} */}
+        <Link href='/admin/report/bookingReport'>
+        Booking Report
+        </Link>
       </div>
     </div>
   );
