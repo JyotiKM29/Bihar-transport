6e862cd819d273909a3d1ed894fb0e0d15c8277b
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-03-07 20:29:52 +0530

    added api to get all the pending invoice booking

---
+++ app/api/accounting/invoice/pending/route.js
@@ -0,0 +1,30 @@
+import Booking from "../../../../models/bookingmodel";
+import user from "../../../../models/usermodel";
+import connectDB from "../../../../middleware/connectDB";
+
+
+export async function POST(req, res) {
+    
+
+    try {
+        const { adminId } = await req.json();
+        await connectDB();
+
+
+        const admin = await user.findOne({ $and: [{ _id: adminId }, { $or: [{ isAdmin: true }, { isOwner: true }] }] });
+        
+        if(!admin){
+            return Response.json({ message: "Admin not found or missing required fields" }, { status: 400 });
+        }
+
+        const booking = await Booking.find({ invoiceStatus: false });
+        console.log(booking.length);
+        return Response.json({ message: "Invoice Generated", booking }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return Response.json({ message: error.message }, { status: 500 })
+    }
+
+
+
+}
