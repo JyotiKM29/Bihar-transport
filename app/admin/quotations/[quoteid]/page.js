aab8124cb36a0e043e6dd94735cb24d2f28a28ba
JyotiKM29 <jyotikumari4442@gmail.com>
2024-03-11 20:41:16 +0530

    Prpduct Name is Not Showing Fixed

---
+++ app/admin/quotations/[quoteid]/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="min-h-[90vh] w-full rounded-2xl  bg-white p-8  shadow-sm "> 
+         <h2 className=" text-3xl font-semibold text-cyan-800 ">
+    Quotations  Details :
+        </h2>
+    </div>
+  )
+}
+
+export default page
