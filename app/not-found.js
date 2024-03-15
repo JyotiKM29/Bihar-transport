2210e13f1f26536e3cf797120bce8e6fe84ae2d8
JyotiKM29 <jyotikumari4442@gmail.com>
2024-03-16 00:00:21 +0530

    refortering

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = () => {
   return (
     <div>
-      <h1>This is a custom 404 page</h1>
+      <h1>This is a custom 404 pages</h1>
       <pre>Sorry, the page you are looking for does not exist.</pre>
     </div>
   );
