cabc017c19ef43726e88b6e2448d4307523c3e96
JyotiKM29 <jyotikumari4442@gmail.com>
2024-04-04 10:34:22 +0530

    Cart is seperated

---
+++ app/admin/booking/CartTable.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { ShoppingCart } from 'lucide-react';
+
+const CartTable = ({items}) => {
+  return (
+    <div className='px-4 py-2 w-full '>
+        <h2 className="font-semibold text-xl flex gap-2 "><ShoppingCart strokeWidth={2.5} /> Total Item in Cart</h2>
+   
+
+ 
+    <div className=' w-full'>
+    { (
+      <table className="mx-2 my-4 w-full border">
+        <thead>
+          <tr className="w-full border bg-slate-50">
+          <th>HSN</th>
+            <th>Material Name</th>
+            <th>Quantity</th>
+            <th>Weight</th>
+            <th>Rate</th>
+            <th>Rate/Per</th>
+            <th>Basic Amount</th>
+           
+            <th>Tax</th>
+            <th>Amount</th>
+             {/* <th>Action
+</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(items) && items.map((items, i) => (
+            <tr key={i} className="w-full text-center">
+              <td>{items.HSN}</td>
+              <td>{items.material}</td>
+              <td>{items.quantity} {items.quantityUnit}</td>
+              <td>{items.actualWeight}{items.actualWeightUnit} </td>
+              {/* <td>{items.chargedWeight}{items.chargedWeightUnit}</td> */}
+              <td>{items.rate}({items.rateUnit})</td>
+              <td>{items.rateAsPer}({items.rateAsPerOption})</td>
+             
+              <td>{items.basicAmount}</td>
+              <td>{ items.GSTPercentage * 100}%</td>
+              <td>{items.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )}
+  </div>
+  </div>
+  )
+}
+
+export default CartTable
