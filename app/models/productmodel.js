7c390d079181ee973e54009a22ac0464f225679c
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-04-04 11:23:16 +0530

    Added model of product

---
+++ app/models/productmodel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    hsnNo: { type: String, required: true }, // Corrected to String type
+}, { timestamps: true });
+
+mongoose.models = {};
+export default mongoose.model("Product", productSchema); // Changed model name to start with capital letter (convention)
