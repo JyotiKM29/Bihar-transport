af53b2d210cd6c02d0cc86f28bafa2c639579200
JyotiKM29 <jyotikumari4442@gmail.com>
2024-04-04 11:02:35 +0530

    resolve error 2

--- app/admin/quotations/[quoteid]/page.js
+++ app/admin/quotations/[quoteid]/page.js
@@ -1,48 +1,40 @@
-// "use client";
-// import React, { useEffect, useState } from "react";
-// import { Button } from "../../../components/ui/button";
-// import { useRouter } from "next/navigation";
-
-// import { IoIosArrowBack } from "react-icons/io";
-// import { MdEdit } from "react-icons/md";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "../../../components/ui/button";
 
 
-// const page = () => {
-//   const [loading, setLoading] = useState(true);
-//   const [data, setData] = useState();
-//   const router = useRouter();
-//   const [editAccounting, setEditAccounting] = useState(false);
+import { IoIosArrowBack } from "react-icons/io";
+import { MdEdit } from "react-icons/md";
 
 
-//   const handleGoBack = () => {
-//     router.back();
-//   };
-//   return (
-//     <div className="min-h-[90vh] w-full rounded-2xl  bg-white p-8  shadow-sm "> 
+const page = () => {
+ 
+  return (
+    <div className="min-h-[90vh] w-full rounded-2xl  bg-white p-8  shadow-sm "> 
         
 
-//         <div className="flex items-center justify-between">
-//         <h2 className=" text-3xl font-semibold text-cyan-800 ">
-//     Quotations  Details :
-//         </h2>
-//         <div className="flex items-center justify-between space-x-2">
-//           <Button className="space-x-2 px-4 bg-cyan-700 hover:bg-cyan-800" onClick={handleGoBack}>
-//             <IoIosArrowBack className=" fill-white" />
-//             <pre className="text-base">Back</pre>
-//           </Button>
-//           <Button
-//             onClick={() => setEditAccounting(!editAccounting)}
-//             className="space-x-2 px-4 bg-cyan-700 hover:bg-cyan-800"
-//           >
-//             <pre className="text-base">Edit</pre>
-//             <MdEdit className="h-8 fill-white" />
-//           </Button>
-//         </div>
-//       </div>
+        <div className="flex items-center justify-between">
+        <h2 className=" text-3xl font-semibold text-cyan-800 ">
+    Quotations  Details :
+        </h2>
+        <div className="flex items-center justify-between space-x-2">
+          <Button className="space-x-2 px-4 bg-cyan-700 hover:bg-cyan-800" >
+            <IoIosArrowBack className=" fill-white" />
+            <pre className="text-base">Back</pre>
+          </Button>
+          <Button
+            onClick={() => setEditAccounting(!editAccounting)}
+            className="space-x-2 px-4 bg-cyan-700 hover:bg-cyan-800"
+          >
+            <pre className="text-base">Edit</pre>
+            <MdEdit className="h-8 fill-white" />
+          </Button>
+        </div>
+      </div>
 
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default page
+export default page
