3809f0e72ea053153e227411845b7321011921a1
JyotiKM29 <jyotikumari4442@gmail.com>
2024-04-14 19:03:05 +0530

    fix error in material info

--- app/admin/booking/MaterialInfo.js
+++ app/admin/booking/MaterialInfo.js
@@ -311,7 +311,7 @@ const MaterialInfo = ({ form, nameValue }) => {
 
                         <FormMessage />
                       </div>
-                    </FormItem>
+                    </FormItem> 
                   );
                 }}
               />
