7c938e24be099a41fb33ddc80b768ec49ea48028
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-04-22 01:45:20 +0530

    Updated the api of get and create for type

--- app/models/type.js
+++ app/models/type.js
@@ -10,39 +10,39 @@ const typeSchema = new mongoose.Schema({
     },
     lenght: {
         type: Number,
-        required: true,
+      
     },
     lenghtUnit: {
         type: String,
-        required: true,
+     
     },
     width: {
         type: Number,
-        required: true,
+     
     },
     widthUnit: {
         type: String,
-        required: true,
+     
     },
     height: {
         type: Number,
-        required: true,
+     
     },
     heightUnit: {
         type: String,
-        required: true,
+     
     },
     weight: {
         type: Number,
-        required: true,
+      
     },
     weightUnit: {
         type: String,
-        required: true,
+     
     },
     capacity: {
         type: Number,
-        required: true,
+       
     },
     addedBy: {
         name: { type: String },
