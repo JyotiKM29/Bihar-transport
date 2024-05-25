6db56d611d4b8e77a525658850c1ced1303ad861
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-05-25 14:17:19 +0530

    Added delete api for all the settings

---
+++ app/api/setting/biltyType/delete/route.js
@@ -0,0 +1,28 @@
+import usermodel from "@/app/models/usermodel";
+import biltyType from "@/app/models/setting/biltyType";
+import connectDB from "@/app/middleware/connectDB";
+
+export async function DELETE(req, res) {
+  try {
+    const { adminId, id } = await req.json();
+    await connectDB();
+
+    const admin = await usermodel.findOne({
+      $and: [{ _id: adminId }, { $or: [{ isAdmin: true }, { isOwner: true }] }],
+    });
+    if (!admin) {
+      return Response.json({ message: "Admin not found" }, { status: 400 });
+    }
+
+    if (!id) {
+      return Response.json({ message: "Trip Type not found" }, { status: 400 });
+    }
+
+    const trip = await biltyType.findByIdAndDelete(id);
+
+    return Response.json({ message: "Deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: error.message }, { status: 400 });
+  }
+}
---
+++ app/api/setting/insurenceProvider/delete/route.js
@@ -0,0 +1,28 @@
+import usermodel from "@/app/models/usermodel";
+import insurenceProvider from "@/app/models/setting/insurenceProvider";
+import connectDB from "@/app/middleware/connectDB";
+
+export async function DELETE(req, res) {
+  try {
+    const { adminId, id } = await req.json();
+    await connectDB();
+
+    const admin = await usermodel.findOne({
+      $and: [{ _id: adminId }, { $or: [{ isAdmin: true }, { isOwner: true }] }],
+    });
+    if (!admin) {
+      return Response.json({ message: "Admin not found" }, { status: 400 });
+    }
+
+    if (!id) {
+      return Response.json({ message: "Trip Type not found" }, { status: 400 });
+    }
+
+    const trip = await insurenceProvider.findByIdAndDelete(id);
+
+    return Response.json({ message: "Deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: error.message }, { status: 400 });
+  }
+}
