918bab4095a5139d67c1ef773f0a5a36baa18e99
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-05-27 11:17:34 +0530

    added api for the payemnt voucher statement

---
+++ app/api/accounting/statementPaymentVoucher/route.js
@@ -0,0 +1,35 @@
+import usermodel from "@/app/models/usermodel";
+import connectDB from "@/app/middleware/connectDB";
+import voucher from "@/app/models/accounting/voucher";
+
+
+export async function POST(req, res) {
+  try {
+    const { adminId, fromDate, toDate } = await req.json();
+
+    await connectDB();
+
+    const admin = await usermodel.findOne({
+      $and: [{ _id: adminId }, { $or: [{ isAdmin: true }, { isOwner: true }] }],
+    });
+
+    if (!admin) {
+      return Response.json({ message: "Admin not found" }, { status: 404 });
+    }
+
+    const from = new Date(fromDate);
+    const to = new Date(toDate);
+
+    const recieptData = await voucher.find({
+      createdAt: {
+        $gte: from,
+        $lt: to,
+      },
+    });
+
+    return Response.json({ recieptData }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: error.message }, { status: 400 });
+  }
+}
